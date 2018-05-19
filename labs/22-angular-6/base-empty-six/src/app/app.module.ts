ef46b8d70bfe88c0ce4e0b69e82af387bb80d32d
Matteo Parodi <matteo.parodi@flairbit.io>
2018-05-19 14:16:40 +0200

    new angular 6 example + @Input() @Output() instead of declarative approach

---
+++ labs/22-angular-6/base-empty-six/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+import { AppComponent } from './app.component';
+import { CommonStarComponent } from './components/common-star/common-star.component';
+import { FolderListComponent } from './components/folder-list/folder-list.component';
+import { MessageListComponent } from './components/message-list/message-list.component';
+import { MessageComposerComponent } from './components/message-composer/message-composer.component';
+import { MailViewComponent } from './mail-view/mail-view.component';
+import { MessageViewerComponent } from './components/message-viewer/message-viewer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CommonStarComponent,
+    MailViewComponent
+  ],
+  imports: [
+    BrowserModule, FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
